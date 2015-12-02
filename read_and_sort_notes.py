d42ee59f5adabcb659d0e22f3134516965ac0697
Ben Kerestan <benkerestan@Connectadick.local>
2015-12-02 13:46:49 -0500

    A few major changes:
    1. There is now a "master" list that contains all the sample chunks. The index of a specific chunk is then stored in a dictionary bin corresponding to the correct note in the correct octave. This helps with chords. Instead of storing a copy of the whole chunk in both the A note bin and the C# note bin, we have one copy of the chunk, and two copies of the chunk's index in the master list.
    
    2. As you may have guessed from the first change, chords now word! There is a variable defined called "threshold" which is the cutoff amplitude value. Frequencies above this value are analyzed and used for sorting the chunks, frequencies below this value are not.

--- read_and_sort_notes.py
+++ read_and_sort_notes.py
@@ -9,7 +9,9 @@ sr = 44100.
 bpm = 148
 dt=0.5
 
-# This dict holds the "ideal" values for each note in the third octave.
+threshold = 1500
+
+# This dict holds the "ideal" values for each note in the Fourth octave.
 # A wave will be said to correspond to a given note if its frequency is within 1%
 # of the ideal values in this table, multiplied appropriately to give the frequency for the correct octave
 note_freqs = {
@@ -30,23 +32,26 @@ note_freqs = {
 #This will hold all our chopped up notes
 # Each note consists of a list containing 5 sublists
 # Each of those 5 sublists corresponds to an octave 1-5
-# Octave 1 is the first element of the list, Octave 5 the fifth
+# Octave 1 is the first element of the list, Octave 6 the sixth
 # For example, notes_db["C"][2] will be a list of all wave chunks whose dominant frequency
 # corresponds to a C3 note, that is, a C in the 3rd octave
 notes_db = {
-"A": [[],[],[],[],[]],
-"A#": [[],[],[],[],[]],
-"B": [[],[],[],[],[]],
-"C": [[],[],[],[],[]],
-"C#": [[],[],[],[],[]],
-"D": [[],[],[],[],[]],
-"D#": [[],[],[],[],[]],
-"E": [[],[],[],[],[]],
-"F": [[],[],[],[],[]],
-"F#": [[],[],[],[],[]],
-"G": [[],[],[],[],[]],
-"G#":[[],[],[],[],[]]
+"A": [[],[],[],[],[],[]],
+"A#": [[],[],[],[],[],[]],
+"B": [[],[],[],[],[],[]],
+"C": [[],[],[],[],[],[]],
+"C#": [[],[],[],[],[],[]],
+"D": [[],[],[],[],[],[]],
+"D#": [[],[],[],[],[],[]],
+"E": [[],[],[],[],[],[]],
+"F": [[],[],[],[],[],[]],
+"F#": [[],[],[],[],[],[]],
+"G": [[],[],[],[],[],[]],
+"G#":[[],[],[],[],[],[]]
 }
+
+
+all_notes=[]
 
 # Use a given tempo to determine the correct length of time for each chunk of wave
 def compute_dt(bpm):
@@ -68,45 +73,89 @@ def split_wave(wave, note_size):
 def store_note(chunk):
 	octave_multiplier = 1
 
-	w = numpy.fft.rfft(chunk)
+	w = numpy.fft.rfft(chunk) / (len(chunk))
+
 	freqs = numpy.fft.fftfreq(len(w))
 	idx = numpy.argmax(numpy.abs(w))
 	frequency = freqs[idx] * (sr/2)
 
-	#determine what octave then note is in
-	# Doubling a frequency increases the note's octave by 1
-	# Thus, if the frequency is outside the default range, simply half or double each bin value to figure out what note it is
-	while frequency < (note_freqs["A"] * octave_multiplier)*0.99:
-		octave_multiplier = octave_multiplier/2.
-	while frequency > (note_freqs["G#"] * octave_multiplier)*1.01:
-		octave_multiplier = octave_multiplier*2
+	#If the loudest frequency is greater than the threshold, store this chunk's index in the appropriate note bin
+	# repeat for the next loudest frequency and so on until the frequencies are no longer louder than the threshold
+	while numpy.abs(w[idx]) > threshold:
+		#determine what octave then note is in
+		# Doubling a frequency increases the note's octave by 1
+		# Thus, if the frequency is outside the default range, simply half or double each bin value to figure out what note it is
+		while frequency < (note_freqs["A"] * octave_multiplier)*0.99:
+			octave_multiplier = octave_multiplier/2.
+		while frequency > (note_freqs["G#"] * octave_multiplier)*1.01:
+			octave_multiplier = octave_multiplier*2
 
-	# iterate over each note in the scale, testing the chunk frequency
-	# If the chunk frequency is within 1% of the note's frequency for a given octave,
-	# place the chunk into that octave's bin within the corresponding note bin
-	for note,note_frequency in note_freqs.iteritems():
-		octave_frequency = note_frequency * octave_multiplier
-		if octave_frequency * 0.99 <= frequency <= octave_frequency*1.01:
-			if octave_multiplier==0.25:
-				notes_db[note][0].append(chunk)
-				return
-			if octave_multiplier==0.5:
-				notes_db[note][1].append(chunk)
-				return
-			if octave_multiplier==1:
-				notes_db[note][2].append(chunk)
-				return
-			if octave_multiplier==2:
-				notes_db[note][3].append(chunk)
-				return
-			if octave_multiplier==4:
-				notes_db[note][4].append(chunk)
-				return
+		# store the note in the list of all stored chunk, and get its index within that list
+		all_notes.append(chunk)
+		note_index = len(all_notes) - 1
 
+		# iterate over each note in the scale, testing the chunk frequency
+		# If the chunk frequency is within 1% of the note's frequency for a given octave,
+		# place the chunk's index into that octave's bin within the corresponding note bin
+		# By storing indices rather than whole chunks here, we can avoid having to store multiple copies of chunks which
+		# represent chords
+		for note,note_frequency in note_freqs.iteritems():
+			octave_frequency = note_frequency * octave_multiplier
+			if octave_frequency * 0.99 <= frequency <= octave_frequency*1.01:
+				if octave_multiplier==0.125 and note_index not in notes_db[note][0]:
+					notes_db[note][0].append(note_index)
+					#return [w, freqs]
+				elif octave_multiplier==0.25 and note_index not in notes_db[note][1]:
+					notes_db[note][1].append(note_index)
+					#return [w, freqs]
+				elif octave_multiplier==0.5 and note_index not in notes_db[note][2]:
+					notes_db[note][2].append(note_index)
+					#return [w,freqs]
+				elif octave_multiplier==1 and note_index not in notes_db[note][3]:
+					notes_db[note][3].append(note_index)
+					#return [w, freqs]
+				elif octave_multiplier==2 and note_index not in notes_db[note][4]:
+					notes_db[note][4].append(note_index)
+					#return [w, freqs]
+				elif octave_multiplier==4 and note_index not in notes_db[note][5]:
+					notes_db[note][5].append(note_index)
+					#return [w, freqs]
+
+		#delete this frequency and its data from w and freqs, then compute a new idx and frequency
+		w = numpy.delete(w,idx)
+		freqs = numpy.delete(freqs,idx)
+		idx = numpy.argmax(numpy.abs(w))
+		frequency = freqs[idx] * (sr/2)
+
+
+
+	
 def print_notes():
-	for i in range(5):
-		for note in notes_db:
-			print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+	for i in range(6):
+		note="A"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+		note="A#"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+		note="B"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+		note="C"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+		note="C#"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+		note="D"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+		note="D#"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+		note="E"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+		note="F"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+		note="F#"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+		note="G"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
+		note="G#"
+		print("{!s}{!s}: {!s}".format(note, i+1, len(notes_db[note][i])))
 
 
 # Tests the sample wave I created in a different function. The first quarter of the file is just a ~440hz sin
@@ -140,6 +189,7 @@ def test_sample_wave(chunks):
 		frequency = freqs[idx] * (sr/2)
 
 		numpy.testing.assert_allclose(frequency, note_freqs["E"], rtol=0.01)
+	print("Tests passed for sample wave!")
 
 if __name__ == '__main__':
 	# figure out the correct dt length based on the tempo of the output wave you want
@@ -156,11 +206,13 @@ if __name__ == '__main__':
 
 	test_sample_wave(wave_chunks)
 
-	for chunk in wave_chunks:
+	for i in range(len(wave_chunks)):
+		chunk = wave_chunks[i]
 		if len(chunk) != int(note_length):
 			continue
 
 		store_note(chunk)
+
 	print_notes()
 
 	
